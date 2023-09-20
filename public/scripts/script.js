36ac03a956a36dc4fe2514dfd557462baf7341d2
Lexor <lesha.goloborodko392@gmail.com>
2023-09-20 15:29:04 +0300

    10. изменения мобильной версии и фикс багов

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -14,59 +14,62 @@ $('a[href*="#"]').click(function () {
     return false;
 });
 
-$(document).ready(function() {
-    function applyParallaxEffect(element, intensity) {
-        var rAF;
-        var currentTransform = 'translate3d(0, 0, 0)';
-        var isMoving = false;
+$(document).ready(function () {
+    if ($(window).width() >= 1080) {
+        function applyParallaxEffect(element, intensity) {
+            var rAF;
+            var currentTransform = 'translate3d(0, 0, 0)';
+            var isMoving = false;
 
-        $(document).mousemove(function(e) {
-            if (element.length === 0) return; // Проверка на существование элемента
+            $(document).mousemove(function (e) {
+                if (element.length === 0) return; // Проверка на существование элемента
 
-            if (!isMoving) {
-                isMoving = true;
+                if (!isMoving) {
+                    isMoving = true;
 
-                var mouseX = e.pageX;
-                var mouseY = e.pageY;
+                    var mouseX = e.pageX;
+                    var mouseY = e.pageY;
 
-                var centerX = element.offset().left + element.width() / 2;
-                var centerY = element.offset().top + element.height() / 2;
+                    var centerX = element.offset().left + element.width() / 2;
+                    var centerY = element.offset().top + element.height() / 2;
 
-                var deltaX = mouseX - centerX;
-                var deltaY = mouseY - centerY;
+                    var deltaX = mouseX - centerX;
+                    var deltaY = mouseY - centerY;
 
-                var percentX = deltaX / (element.width() / 2);
-                var percentY = deltaY / (element.height() / 2);
+                    var percentX = deltaX / (element.width() / 2);
+                    var percentY = deltaY / (element.height() / 2);
 
-                var parallaxX = percentX * intensity;
-                var parallaxY = percentY * intensity;
+                    var parallaxX = percentX * intensity;
+                    var parallaxY = percentY * intensity;
 
-                var newTransform = 'translate3d(' + parallaxX + 'px,' + parallaxY + 'px, 0)';
+                    var newTransform = 'translate3d(' + parallaxX + 'px,' + parallaxY + 'px, 0)';
 
-                if (newTransform !== currentTransform) {
-                    currentTransform = newTransform;
+                    if (newTransform !== currentTransform) {
+                        currentTransform = newTransform;
 
-                    cancelAnimationFrame(rAF);
-                    rAF = requestAnimationFrame(function() {
-                        // Используем transform для плавного движения
-                        element.css({
-                            'transform': newTransform
+                        cancelAnimationFrame(rAF);
+                        rAF = requestAnimationFrame(function () {
+                            // Используем transform для плавного движения
+                            element.css({
+                                'transform': newTransform
+                            });
+                            isMoving = false;
                         });
-                        isMoving = false;
-                    });
+                    }
                 }
-            }
-        });
-    }
-    // Применить эффект к .circle с интенсивностью 20
-    applyParallaxEffect($('.moove20'), 10);
+            });
+        }
+        // Применить эффект к .circle с интенсивностью 20
+        applyParallaxEffect($('.moove20'), 10);
 
-    // Применить эффект к .rectangle с интенсивностью 10
-    applyParallaxEffect($('.moove10'), 8);
-    applyParallaxEffect($('.moove2'), 4);
-    applyParallaxEffect($('.moove1'), 1);
+        // Применить эффект к .rectangle с интенсивностью 10
+        applyParallaxEffect($('.moove10'), 8);
+        applyParallaxEffect($('.moove2'), 4);
+        applyParallaxEffect($('.moove1'), 1);
 
-    
+
+    } else {}
+
     function rotateTags() {
         var $tags = $(".container-people-add .tag");
         var $activeTag = $tags.filter(".active");
@@ -92,14 +95,18 @@ $(document).ready(function() {
 
     // Вызываем функцию rotateTags() каждые 4 секунды
     setInterval(rotateTags, 4000);
-    
+
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     var offcanvasNavbar = $("#offcanvasNavbar");
     var navLinks = offcanvasNavbar.find(".nav-link");
 
-    navLinks.on("click", function() {
+    navLinks.on("click", function () {
         offcanvasNavbar.offcanvas("hide");
     });
+});
+$(document).ready(function () {
+    $(".coin-marquee-header").hide();
+    $(".coin-marquee-container").addClass('container d-flex justify-content-center');
 });
